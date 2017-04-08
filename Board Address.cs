d902fa4c3e43085d916b10d8c29eed2f2dae694d
jack <jssgh6@mst.edu>
2017-04-08 14:10:27 -0500

    implemented Index property added static constructor to setup PositionLookup and IndexLookup

--- Board Address.cs
+++ Board Address.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LittleOwl {
     public class BoardAddress {
-        public char File { get; }
+        public char File { get { return (char)('a' + (int)(Position / 8)); } }
         public int Rank { get; }
+
+        private static ulong[] PositionLookup;
+        private static Dictionary<ulong, int> IndexLookup;
+        static BoardAddress() {
+            PositionLookup = new ulong[64];
+            for (uint i = 0; i < 64; i++)
+                PositionLookup[i] = Utilities.Pow(2, i);
+
+            IndexLookup = new Dictionary<ulong, int>();
+            ulong Bitboard = 1;
+            for (int i = 0; i < 64; i++) {
+                IndexLookup.Add(Bitboard, i);
+                Bitboard <<= 1;
+            }
+        }
+
+        public int Index { get { return IndexLookup[Position]; } set { Position = PositionLookup[value]; } }
 
         private ulong _Position;
         public ulong Position {
@@ -15,6 +33,8 @@ namespace LittleOwl {
         }
 
         public BoardAddress(ulong pos) { Position = pos; }
+
+        public BoardAddress(int i) { Index = i; } // todo is this bad form? (two constructors with single numeric args)
 
         public BoardAddress(char f, int r) {
             f = char.ToLower(f);
