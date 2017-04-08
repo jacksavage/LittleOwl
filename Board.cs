8a68c17863a6815fa55f853e028a47aab5f1ffb3
jack <jssgh6@mst.edu>
2017-04-08 08:53:17 -0500

    started implementing Board and PiecePositions classes porting code embedded in the Joeuer solution (my implementation from CS5400)

---
+++ Board.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace LittleOwl {
+    // fully represents a state in a chess game
+    public class Board {
+        // create a board with a FEN string
+        public Board(string fen) {
+            Match Match = FenParser.Match(fen);
+            if (!Match.Success) throw new ArgumentException("invalid FEN string");
+            string[] Fields = {Match.Groups[1].Value, Match.Groups[2].Value, Match.Groups[3].Value, Match.Groups[4].Value, Match.Groups[5].Value, Match.Groups[6].Value};
+
+        }
+
+        // create a FEN string from the calling board
+        public override string ToString() {
+            var Result = new StringBuilder();
+
+            return Result.ToString();
+        }
+
+        private const string FenPattern = @"(?i)^\s*([pnbrkq1-8\/]{17,})\s+([wb])\s+([kq\-]{4})\s+((?:[a-h][1-8])|\-)\s+(\d{1,2})\s+(\d+)\s*$";
+        private static Regex FenParser = new Regex(FenPattern);
+
+        internal PiecePositions Pieces;
+        internal bool ActiveColorWhite;
+        internal ulong EnPassantTarget;
+        // todo internal CastlingAvailability;
+        internal byte HalfMoveClock;
+        internal int FullMoveNumber;
+    }
+}
---
+++ Piece Positions.cs
@@ -0,0 +1,39 @@
+﻿namespace LittleOwl {
+    internal class PiecePositions {
+        public PiecePositions() {
+            Black = new Player(this);
+            White = new Player(this);
+        }
+
+        private ulong _Pawns;
+        private ulong _Knights;
+        private ulong _Bishops;
+        private ulong _Rooks;
+        private ulong _Queens;
+        private ulong _Kings;
+
+        public ulong Pawns { get { return _Pawns; } set { _Pawns = value; } }
+        public ulong Knights { get { return _Knights; } set { _Knights = value; } }
+        public ulong Bishops { get { return _Bishops; } set { _Bishops = value; } }
+        public ulong Rooks { get { return _Rooks; } set { _Rooks = value; } }
+        public ulong Queens { get { return _Queens; } set { _Queens = value; } }
+        public ulong Kings { get { return _Kings; } set { _Kings = value; } }
+        public ulong All { get { return Black.All & White.All; } } // todo PiecePosition.All setter
+        public Player Black;
+        public Player White;
+
+        public class Player {
+            public Player(PiecePositions p) { Parent = p; }
+            public ulong All { get { return _All; } } // todo Player.All setter
+            public ulong Pawns { get { return _All & Parent.Pawns; } }
+            public ulong Knights { get { return _All & Parent.Knights; } }
+            public ulong Bishops { get { return _All & Parent.Bishops; } }
+            public ulong Rooks { get { return _All & Parent.Rooks; } }
+            public ulong Queens { get { return _All & Parent.Queens; } }
+            public ulong King { get { return _All & Parent.Queens; } }
+
+            private ulong _All;
+            private PiecePositions Parent;
+        }
+    }
+}
