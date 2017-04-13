5f012be942ce6abfc460c2fedcdbe4fe641a12d0
jack <jssgh6@mst.edu>
2017-04-13 07:55:57 -0500

    moved the Masks class to its own file

--- Engine.cs
+++ Engine.cs
@@ -79,32 +79,4 @@ namespace LittleOwl {
             return Result;
         }
     }
-
-    internal static class Masks {
-        static Masks() {
-            // todo setup masks
-        }
-
-        // todo file mask constants
-        public const ulong FileA = 0;
-        public const ulong FileB = 0;
-        public const ulong FileC = 0;
-        public const ulong FileD = 0;
-        public const ulong FileE = 0;
-        public const ulong FileF = 0;
-        public const ulong FileG = 0;
-        public const ulong FileH = 0;
-
-        // todo rank mask constants
-        public const ulong Rank1 = 0;
-        public const ulong Rank2 = 0;
-        public const ulong Rank3 = 0;
-        public const ulong Rank4 = 0;
-        public const ulong Rank5 = 0;
-        public const ulong Rank6 = 0;
-        public const ulong Rank7 = 0;
-        public const ulong Rank8 = 0;
-
-        public const ulong Rank27 = Rank2 | Rank7;
-    }
 }
