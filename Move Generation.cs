124ce0c15d2b1b5be08821a89331589e74fe868b
jack <jssgh6@mst.edu>
2017-04-23 14:31:28 -0500

    added a MoveBoardPair struct to return both the Move and the Board since the Board needs to be generated anyways to make sure that the current player is not moving into check this keeps the Searcher from having to regenerate the resulting Board state added method that generates each move from a given board and a method that returns all of the moves in one array (the former still needs some cleanup)

--- Move Generation.cs
+++ Move Generation.cs
@@ -1,5 +1,6 @@
 ﻿namespace LittleOwl {
     using System;
+    using System.Collections.Generic;
 
     internal static class MoveGen {
         // all of the spaces a given player can capture {
@@ -15,6 +16,37 @@
             Result |= QueenMoveMap(player);
             Result |= TheKingMoveMap(player);
             return Result;
+        }
+
+        // get the moves/boards one by one
+        public static IEnumerable<MoveBoardPair> EachMove(Board board) {
+            var Buffer = new List<Move>();
+            Board NextBoard;
+
+            // add all of the moves to the buffer
+            Buffer.AddRange(PawnMoves(board));
+            Buffer.AddRange(KnightMoves(board));
+            Buffer.AddRange(BishopMoves(board));
+            Buffer.AddRange(RookMoves(board));
+            Buffer.AddRange(QueenMoves(board));
+            Buffer.AddRange(KingMoves(board));
+
+            // look at pawn moves
+            foreach (Move move in Buffer) {
+                // get next board state by applying this move to the current board
+                NextBoard = Engine.ApplyMove(board, move);
+
+                // if move doesn't put friendly king in check then yield it
+                if (!NextBoard.InCheck) yield return new MoveBoardPair(NextBoard, move);
+            }
+        }
+
+        // get all of the moves/boards at once
+        public static MoveBoardPair[] AllMoves(Board board) {
+            var Result = new Queue<MoveBoardPair>();
+            foreach (MoveBoardPair mb in Moves(board))
+                Result.Enqueue(mb);
+            return Result.ToArray();
         }
 
         private static ulong TheKingMoveMap(PiecePositions.Player player) {
@@ -39,6 +71,15 @@
 
         private static ulong PawnMoveMap(PiecePositions.Player player) {
             throw new NotImplementedException();
+        }
+
+        public struct MoveBoardPair {
+            public MoveBoardPair(Board board, Move move) {
+                this.Board = board;
+                this.Move = move;
+            }
+            public Board Board;
+            public Move Move;
         }
     }
 }
