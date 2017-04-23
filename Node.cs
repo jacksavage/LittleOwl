ade187005dbbc1e3628d5c156e4ea33404f2aef9
jack <jssgh6@mst.edu>
2017-04-23 13:41:23 -0500

    moved a ton of Board eval functionality into a new Searcher.Node class made Searcher a partial class and put the new Node class in its own file

---
+++ Node.cs
@@ -0,0 +1,127 @@
+﻿namespace LittleOwl {
+    using System;
+    using System.Collections.Generic;
+
+    internal partial class Searcher {
+        private class Node {
+            // for root node (leave Parent/LastMove null)
+            public Node(Board board) { this.Board = board; }
+
+            // for every other node
+            public Node(Node parent, Board board, Move lastMove) {
+                this.Parent = parent;
+                this.Board = board;
+                this.LastMove = lastMove;
+            }
+
+            // does the active player have insufficient material?
+            private bool InsufficientMaterial {
+                get {
+                    if ((Board.ActivePlayer.Pawns | Board.ActivePlayer.Rooks | Board.ActivePlayer.Queens) == 0) {
+                        int NumKnights = Utilities.NumActiveBits(Board.ActivePlayer.Knights);
+                        int NumBishops = Utilities.NumActiveBits(Board.ActivePlayer.Bishops);
+
+                        if (NumBishops == 1 && NumKnights == 0) return true; // king and a bishop
+                        if (NumBishops == 0 && NumKnights == 1) return true; // king and knight
+                        if (NumBishops == 0 && NumKnights == 0) return true; // only a king remaining
+                    }
+
+                    return false;
+                }
+            }
+
+            // check for simplified three move repetition
+            private bool MoveRepetitionOccurred {
+                get {
+                    if (Board.PastMoves.Count < 8) return false;
+
+                    Move[] Moves = Board.PastMoves.ToArray();
+                    if (Moves[0] == Moves[4] &&
+                        Moves[1] == Moves[5] &&
+                        Moves[2] == Moves[6] &&
+                        Moves[3] == Moves[7]) return true;
+
+                    return false;
+                }
+            }
+
+            // is the active player in draw?
+            private bool InDraw {
+                get {
+                    if (Board.HalfMoveClock < 1) return true; // half move clock ran out
+                    if (InsufficientMaterial) return true; // insufficient material
+                    if (MoveRepetitionOccurred) return true; // move repetition
+                    if (MoveGen.MoveMap(Board.ActivePlayer) == 0) return true; // stalemate
+                    return false; // not in draw
+                }
+            }
+
+            // is a given player in check?
+            private bool InCheck { get { return (MoveGen.MoveMap(Board.InactivePlayer) & Board.ActivePlayer.King) != 0; } }
+
+            // is the active player in check mate?
+            private bool InCheckMate { get { return InCheck && (MoveGen.MoveMap(Board.ActivePlayer) == 0); } }
+
+            // is this an end-of-game state?
+            public bool Terminal { get { return InCheckMate || InDraw; } }
+
+            // the material value for a given player
+            private int MaterialValue(PiecePositions.Player player) {
+                int Result = 0;
+                foreach (ulong pawn in Utilities.BitSplit(player.Pawns)) Result += PawnValue;
+                foreach (ulong knight in Utilities.BitSplit(player.Knights)) Result += KnightValue;
+                foreach (ulong bishop in Utilities.BitSplit(player.Knights)) Result += BishopValue;
+                foreach (ulong rook in Utilities.BitSplit(player.Knights)) Result += RookValue;
+                foreach (ulong queen in Utilities.BitSplit(player.Knights)) Result += QueenValue;
+                return Result;
+            }
+
+            // get the material advantage for the active top level player
+            private int MaterialAdvantage {
+                get {
+                    if (TopLevelActivePlayerWhite) return MaterialValue(Board.Pieces.White) - MaterialValue(Board.Pieces.Black);
+                    else return MaterialValue(Board.Pieces.Black) - MaterialValue(Board.Pieces.White);
+                }
+            }
+
+            // is this an inactive state?
+            public bool Quiescent {
+                get {
+                    throw new NotImplementedException(); // todo Searcher.Node.Quiescent propert
+                }
+            }
+
+            // next possible board states
+            public IEnumerable<Node> Children {
+                get {
+                    throw new NotImplementedException(); // todo Searcher.Node.Children property
+                }
+            }
+
+            // the utility of a given board to the active player
+            public int Utility {
+                get {
+                    if (InCheckMate) {
+                        if (Board.ActiveColorWhite == TopLevelActivePlayerWhite) return int.MinValue; // top level player in check mate
+                        else return int.MaxValue; // top level player's opponent is in check mate 
+                    }
+
+                    if (InDraw) return int.MinValue + 1; // as close to a loss as you can get (for either player)
+
+                    return MaterialAdvantage; // for top level active player
+                }
+            }
+
+            private Node Parent;
+            public Board Board;
+            public Move LastMove = null;
+            public static bool TopLevelActivePlayerWhite;
+
+            private const int PawnValue = 1; // todo store this in external settings
+            private const int KnightValue = 3;
+            private const int BishopValue = 3;
+            private const int RookValue = 5;
+            private const int QueenValue = 9;
+        }
+    }
+}
--- Searcher.cs
+++ Searcher.cs
@@ -1,9 +1,8 @@
 ﻿namespace LittleOwl {
     using System;
-    using System.Collections.Generic;
     using System.Timers;
 
-    internal class Searcher {
+    internal partial class Searcher {
         public Searcher() {
             Countdown = new Timer();
             Countdown.Elapsed += ClockElapsed; // attach handler to elapsed event
@@ -20,18 +19,20 @@
             Countdown.Interval = time.TotalMilliseconds;
             Countdown.Start();
 
-            WhiteAtMove = root.ActiveColorWhite; // store for utility eval
+            Node RootNode = new Node(root);
+            Node.TopLevelActivePlayerWhite = root.ActiveColorWhite; // store for utility eval
+
             Move Result = null;
             int Value = int.MinValue;
             int NextDepth;
 
             for (int d = 1; d <= depth; d++) { // step through depths
                 NextDepth = d - 1;
-                foreach (BoardMovePair child in Children(root)) { // step through children
-                    if (Result == null) Result = child.move; // default to first move
+                foreach (Node child in RootNode.Children) { // step through children
+                    if (Result == null) Result = child.LastMove; // default to first move
                     if (Timeout) return Result; // return best move if timed out
-                    Value = MinV(child.board, NextDepth, qDepth, alpha, beta); // get minimized value from new board
-                    if (Value > alpha) { alpha = Value; Result = child.move; } // update alpha and best move if better val found
+                    Value = MinV(child, NextDepth, qDepth, alpha, beta); // get minimized value from new board
+                    if (Value > alpha) { alpha = Value; Result = child.LastMove; } // update alpha and best move if better val found
                 }
             }
 
@@ -39,142 +40,40 @@
         }
 
         // find minimized utility relative to the active player from a given board
-        private int MinV(Board board, int depth, int qDepth, int alpha, int beta) {
-            if (Timeout || depth == 0 || Terminal(board)) return Utility(board);
+        private int MinV(Node n, int d, int q, int a, int b) {
+            if (Timeout || d == 0 || n.Terminal) return n.Utility;
 
             int Value = int.MaxValue;
-            int NextDepth = depth - 1;
+            int NextDepth = d - 1;
 
-            foreach (BoardMovePair child in Children(board)) {
-                Value = MaxV(child.board, NextDepth, qDepth, alpha, beta);
-                if (Value <= alpha) return Value; // failed low (prune)
-                if (Value < beta) beta = Value;
+            foreach (Node child in n.Children) {
+                Value = MaxV(child, NextDepth, q, a, b);
+                if (Value <= a) return Value; // failed low (prune)
+                if (Value < b) b = Value;
             }
 
             return Value;
         }
 
         // find maximized utility relative to the active player from a given board
-        private int MaxV(Board board, int depth, int qDepth, int alpha, int beta) {
-            if (Timeout || depth == 0 || Terminal(board)) return Utility(board);
+        private int MaxV(Node n, int d, int q, int a, int b) {
+            if (Timeout || d == 0 || n.Terminal) return n.Utility;
 
             int Value = int.MinValue;
-            int NextDepth = depth - 1;
+            int NextDepth = d - 1;
 
-            foreach (BoardMovePair child in Children(board)) {
-                Value = MinV(child.board, NextDepth, qDepth, alpha, beta);
-                if (Value >= beta) return Value; // failed high (prune)
-                if (Value > alpha) alpha = Value;
+            foreach (Node child in n.Children) {
+                Value = MinV(child, NextDepth, q, a, b);
+                if (Value >= b) return Value; // failed high (prune)
+                if (Value > a) a = Value;
             }
 
             return Value;
         }
 
-        private IEnumerable<BoardMovePair> Children(Board board) {
-            throw new NotImplementedException();
-        }
-
-        // is this an end-of-game state?
-        private bool Terminal(Board board) { return InCheckMate(board) || InDraw(board); }
-
-        // the utility of a given board to the active player
-        private int Utility(Board board) {
-            if (InCheckMate(board)) return int.MinValue;
-            if (InDraw(board)) return int.MinValue + 1;
-            return MaterialAdvantage(board);
-        }
-
-        // is this an inactive state?
-        private bool Quiescent(Board board) {
-            throw new NotImplementedException();
-        }
-
-        private bool InCheck(Board board) {
-            throw new NotImplementedException();
-        }
-
-        // is the active player in check mate?
-        private bool InCheckMate(Board board) {
-            throw new NotImplementedException();
-        }
-
-        // is the active player in draw?
-        private bool InDraw(Board board) {
-            // no pawn move or capture
-            if (board.HalfMoveClock < 1) return true;
-
-            // insufficient material
-            if (WhiteAtMove) { if (InsufficientMaterial(board.Pieces.White)) return true; }
-            else { if (InsufficientMaterial(board.Pieces.Black)) return true; }
-
-            // move repetition
-            if (MoveRepetitionOccurred(board)) return true;
-
-            // stalemate
-            if (WhiteAtMove) { if (MoveGen.MoveMap(board.Pieces.White) == 0) return true; }
-            else { if (MoveGen.MoveMap(board.Pieces.Black) == 0) return true; }
-
-            return false;
-        }
-
-        // does the active player have insufficient material?
-        private bool InsufficientMaterial(PiecePositions.Player player) {
-            if ((player.Pawns | player.Rooks | player.Queens) == 0) {
-                int NumKnights = Utilities.NumActiveBits(player.Knights);
-                int NumBishops = Utilities.NumActiveBits(player.Bishops);
-
-                if (NumBishops == 1 && NumKnights == 0) return true; // king and a bishop
-                if (NumBishops == 0 && NumKnights == 1) return true; // king and knight
-                if (NumBishops == 0 && NumKnights == 0) return true; // only a king remaining
-            }
-
-            return false;
-        }
-
-        // check for simplified three move repetition
-        private bool MoveRepetitionOccurred(Board board) {
-            if (board.PastMoves.Count < 8) return false;
-
-            Move[] Moves = board.PastMoves.ToArray();
-            if (Moves[0] == Moves[4] &&
-                Moves[1] == Moves[5] &&
-                Moves[2] == Moves[6] &&
-                Moves[3] == Moves[7]) return true;
-
-            return false;
-        }
-
-        // the material advantange of the active player
-        private int MaterialAdvantage(Board board) {
-            if (WhiteAtMove) return MaterialValue(board.Pieces.White) - MaterialValue(board.Pieces.Black);
-            else return MaterialValue(board.Pieces.Black) - MaterialValue(board.Pieces.White);
-        }
-
-        // the material value for a player
-        private int MaterialValue(PiecePositions.Player player) {
-            int Result = 0;
-            foreach (ulong pawn in Utilities.BitSplit(player.Pawns)) Result += PawnValue;
-            foreach (ulong knight in Utilities.BitSplit(player.Knights)) Result += KnightValue;
-            foreach (ulong bishop in Utilities.BitSplit(player.Knights)) Result += BishopValue;
-            foreach (ulong rook in Utilities.BitSplit(player.Knights)) Result += RookValue;
-            foreach (ulong queen in Utilities.BitSplit(player.Knights)) Result += QueenValue;
-            return Result;
-        }
-
-        private const int PawnValue = 1; // todo store this in external settings
-        private const int KnightValue = 3;
-        private const int BishopValue = 3;
-        private const int RookValue = 5;
-        private const int QueenValue = 9;
-
         private bool WhiteAtMove;
         private Timer Countdown;
         private bool Timeout;
         private void ClockElapsed(object sender, ElapsedEventArgs e) { Timeout = true; }
-
-        private struct BoardMovePair {
-            public Board board;
-            public Move move;
-        }
     }
 }
